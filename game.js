21378eef7ccc7a698477c3bcf7e00abd09f4bcc1
Josy Hartig <josy_hartig@yahoo.de>
2019-06-22 16:10:54 +0800

    added basic functionality

--- game.js
+++ game.js
@@ -1,1 +1,50 @@
-alert('HIIIIII')
+var gamePattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $('#' + randomChosenColor).fadeIn(100).fadeout(100).fadeIn(100);
+  playSound(randomChosenColor);
+  animatePress(randomChosenColor);
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+$(".btn").click(function() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+});
+
+$(document).one('keypress', function() {
+  nextSequence();
+  var level = 0;
+});
+
+function checkAnswer(currentLevel)
