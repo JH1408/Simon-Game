0c6aee8605e86534e2eb897256dee03de6b5e07f
Josy Hartig <josy_hartig@yahoo.de>
2019-06-27 09:38:05 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-var gamePattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
 var userClickedPattern = [];
 var started = false;
 var level = 0;
@@ -18,6 +18,7 @@ function playSound(name) {
 }
 
 function nextSequence() {
+  userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
@@ -40,6 +41,7 @@ $(".btn").click(function() {
   userClickedPattern.push(userChosenColor);
   playSound(userChosenColor);
   animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length-1);
 });
 
 $(document).one('keypress', function() {
@@ -47,4 +49,15 @@ $(document).one('keypress', function() {
   var level = 0;
 });
 
-function checkAnswer(currentLevel)
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length){
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+  }
+}
